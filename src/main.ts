bb5863c91dd1300e5c9b69299bf9cec1829666cf
HamzaOubada2 <oubham220@gmail.com>
2026-03-15 18:09:41 +0000

    config database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // ─── Security ───
+  app.use(helmet())
+
+  // ─── Global prefix ───
+  app.setGlobalPrefix('api/v1')
+
+
+  // ─── Global Pipes ───
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  )
+
+  // ─── Swagger ───
+  const config = new DocumentBuilder()
+    .setTitle('Smart LMS API')
+    .setDescription('Learning Management System — NestJS')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Server running on http://localhost:${port}`);
+  console.log(`📚 Swagger docs: http://localhost:${port}/api/docs`);
 }
 bootstrap();
